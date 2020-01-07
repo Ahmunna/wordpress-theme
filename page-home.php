9614db62ced017f70c992709a3bf2cb6ade550f6
Adnane Hmunna <adn.hmunna@gmail.com>
2020-01-07 13:03:35 +0100

    Activités Section dynamic

--- page-home.php
+++ page-home.php
@@ -4,31 +4,43 @@
  */
 
 get_header();
+
+//Advanced Custom Fields
+$description_activite = get_field('description_activite');
+$photo_activite_1 = get_field('photo_activite_1');
+$titre_activite_1 = get_field('titre_activite_1');
+$description_activite_1 = get_field('description_activite_1');
+$photo_activite_2 = get_field('photo_activite_2');
+$titre_activite_2 = get_field('titre_activite_2');
+$description_activite_2 = get_field('description_activite_2');
+$photo_activite_3 = get_field('photo_activite_3');
+$titre_activite_3 = get_field('titre_activite_3');
+$description_activite_3 = get_field('description_activite_3');
 ?>
 <!--Activities Section-->
 <div class="container text-center">
     <h2>Nos activités</h2>
-    <p>Le club propose plusieurs activités à ses adhérents</p>
+    <p><?php echo $description_activite; ?></p>
     <div class="d-flex justify-content-center align-items-center flex-wrap">
-        <div class="card bg-light mb-3 ml-4 mr-4" style="max-width: 18rem;">
-            <div class="card-header">Header</div>
-            <div class="card-body">
-                <h5 class="card-title">Light card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div class="card bg-white mb-3 ml-4 mr-4" style="max-width: 20rem;">
+            <div class="card-body ">
+                <img src="<?php echo $photo_activite_1['url']?>" class="rounded mx-auto w-25 m-4 d-block"  alt="<?php echo $photo_activite_1['alt']; ?>">
+                <h5 class="card-title m-4"><?php echo $titre_activite_1; ?></h5>
+                <p class="card-text m-3"><?php echo $description_activite_1 ; ?></p>
             </div>
         </div>
-        <div class="card bg-light mb-3 ml-4 mr-4" style="max-width: 18rem;">
-            <div class="card-header">Header</div>
-            <div class="card-body">
-                <h5 class="card-title">Light card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div class="card bg-white mb-3 ml-4 mr-4" style="max-width: 20rem;">
+            <div class="card-body ">
+                <img src="<?php echo $photo_activite_2['url']?>" class="rounded mx-auto w-25 m-4 d-block"  alt="<?php echo $photo_activite_2['alt']; ?>">
+                <h5 class="card-title m-4"><?php echo $titre_activite_2; ?></h5>
+                <p class="card-text m-3"><?php echo $description_activite_2 ; ?></p>
             </div>
         </div>
-        <div class="card bg-light mb-3 ml-4 mr-4" style="max-width: 18rem;">
-            <div class="card-header">Header</div>
-            <div class="card-body">
-                <h5 class="card-title">Light card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div class="card bg-white mb-3 ml-4 mr-4" style="max-width: 20rem;">
+            <div class="card-body ">
+                <img src="<?php echo $photo_activite_3['url']?>" class="rounded mx-auto w-25 m-4 d-block"  alt="<?php echo $photo_activite_3['alt']; ?>">
+                <h5 class="card-title m-4"><?php echo $titre_activite_3; ?></h5>
+                <p class="card-text m-3"><?php echo $description_activite_3 ; ?></p>
             </div>
         </div>
     </div>
